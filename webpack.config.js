9a7d54d349919acd9bc34e07ac989b0751d7864e
santirequelme <65666330+santirequelme@users.noreply.github.com>
2022-05-12 15:55:40 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  mode: isDevelopment ? "development" : "production",
+  devtool: isDevelopment ? "eval-source-map" : "souce-map", //sourcemap
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  devServer: {
+    static: path.resolve(__dirname, "public"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }, {loader: 'sass-loader'}],
+      },
+    ],
+  },
+};
